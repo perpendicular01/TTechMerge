a55758164624a142cf9c8348362119a47aa55d0d
saif <kh.s.k.rohan@gmail.com>
2025-04-28 11:22:31 +0600

    final merge all and done

--- controllers/scrapeMonitor.js
+++ controllers/scrapeMonitor.js
@@ -425,6 +425,226 @@ exports.scrapeTechlandMoni = async () => {
 
 
 
+exports.scrapePcHouseMoni = async () => {
+    var browser, page, links1, photos1, links2, photos2, links, photos, specification, productName, price, resolution, resolutionMatch, displaySize, panelType, numericValues, warranty, description;
+
+    try {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('https://www.pchouse.com.bd/monitor?sort=p.sort_order&order=ASC&fq=1&limit=100');
+
+
+        links1 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a', (links) => {
+            return links.map(x => x.href);
+        })
+
+
+        photos1 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a > div > img', (elements) => {
+            return elements.map(element => element.src);
+        })
+
+        await page.goto('https://www.pchouse.com.bd/monitor?sort=p.sort_order&order=ASC&fq=1&limit=100&page=2');
+
+        links2 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a', (links) => {
+            return links.map(x => x.href);
+        })
+
+
+        photos2 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a > div > img', (elements) => {
+            return elements.map(element => element.src);
+        })
+
+        await page.goto('https://www.pchouse.com.bd/monitor?sort=p.sort_order&order=ASC&fq=1&limit=100&page=3');
+        links3 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a', (links) => {
+            return links.map(x => x.href);
+        })
+
+
+        photos3 = await page.$$eval('#content > div.main-products-wrapper > div.main-products.product-grid > div:nth-child(n) > div > div.image > a > div > img', (elements) => {
+            return elements.map(element => element.src);
+        })
+
+        links = [...links1, ...links2, ...links3];
+        photos = [...photos1, ...photos2, ...photos3];
+    }
+    catch (error) {
+        console.error(error);
+        console.log('monitor-pchouse');
+    }
+
+    //Contains src of the images
+
+    i = 0;
+    for (link of links) {
+        try {
+            await page.goto(link);
+
+            specification = await page.$$eval('#tab-specification > div > table', (elements) => {
+                return elements.map(x => x.textContent.trim());
+            })
+
+            if (specification.length <= 0) {
+                specification = await page.$$eval('#blocks--tab-1 > div > div > div > table > tbody', (elements) => {
+                    return elements.map(x => x.textContent.trim());
+                })
+            }
+
+            if (specification.length <= 0) {
+                specification = await page.$$eval('#blocks--tab-1 > div > div > div > table:nth-child(2) > tbody', (elements) => {
+                    return elements.map(x => x.textContent.trim());
+                })
+            }
+
+            if (specification.length <= 0) {
+                i++;
+                continue;
+            }
+
+            //console.log(specification[0]);
+
+            productName = await page.$$eval('#product > div.title.page-title', (elements) => {
+                return elements.map((element) => {
+                    // Use String.replace() to remove newline characters
+                    return element.textContent.replace(/\n/g, '');
+                });
+            });
+
+
+
+            price = await page.$$eval('#product > div.product-price-group > div.price-wrapper > div.price-group > div > div > div > div.module-item.module-item-1.no-expand > div > div.block-header > span > div', (elements) => {
+                return elements.map((element) => {
+                    // Use String.replace() to remove newline characters
+                    temp = element.textContent.replace(/\n/g, '');
+                    temp = temp.replace(/,/g, '').replace('৳', '');
+                    return parseInt(temp);
+                });
+            });
+            if (isNaN(price) || price == undefined) {
+                i++;
+                continue;
+            }
+
+
+
+            //Assuming specification[0] contains the specifications string
+            resolution;
+            resolutionMatch = specification[0].match(/(?:1920x1080|1920\s*X\s*1080|3840x2160|3840\s*X\s*2160|2560x1440|2560\s*X\s*1440)/i);
+            if (resolutionMatch) {
+                resolution = resolutionMatch[0]; // Extract the matched resolution value
+                resolution = resolution.replace(/\s+/g, ''); // Remove spaces in the matched resolution value
+                resolution = resolution.replace('X', 'x');
+            } else {
+                i++;
+                continue;
+            }
+
+
+            displaySize;
+            displaySize = specification[0].substr(specification[0].indexOf('Display Size') + 11, 50);
+
+            // Use regular expression to extract numeric values from the string
+            numericValues = displaySize.match(/\d+(\.\d+)?/g);
+
+            // If there are numeric values, you can use them as needed
+            if (numericValues) {
+                // numericValues is an array containing the extracted numeric values
+                displaySize = parseFloat(numericValues) // Output: [ '15.6' ] (for example)
+            } else {
+                displaySize = specification[0].substr(specification[0].indexOf('Screen Size') + 11, 50);
+                numericValues = displaySize.match(/\d+(\.\d+)?/g);
+
+                // If there are numeric values, you can use them as needed
+                if (numericValues) {
+                    // numericValues is an array containing the extracted numeric values
+                    displaySize = parseFloat(numericValues) // Output: [ '15.6' ] (for example)
+                } else {
+                    i++;
+                    continue;
+                }
+            }
+
+            if (displaySize > 50) {
+                i++;
+                continue;
+            }
+
+
+
+            panelType;
+            let specificationText = specification[0].toLowerCase();
+            if (/ips\b/i.test(specificationText)) {
+                panelType = 'IPS';
+            } else if (/va\b/i.test(specificationText)) {
+                panelType = 'VA';
+            } else if (/tn\b/i.test(specificationText)) {
+                panelType = 'TN';
+            } else if (/oled\b/i.test(specificationText)) {
+                panelType = 'OLED';
+            } else {
+                panelType = 'Unmentioned'; // Assign 'Unmentioned' if none of the panel types are specified
+            }
+
+
+            warranty;
+            specification[0] = specification[0].replace('Warranty Information', '');
+            warranty = specification[0].substr(specification[0].indexOf('Warranty') + 8, 30);
+            warranty.replace('0', '');
+            warranty = parseInt(warranty);
+            if (isNaN(warranty))
+                warranty = 3;
+
+
+            description = await page.$$eval('#blocks-67326f83b2271-tab-2 > div > div > div.block-content.block-description', (elements) => {
+                return elements.map((element) => {
+                    // Use String.replace() to remove newline characters
+                    return element.textContent.trim();
+                });
+            });
+
+            if (description == undefined) {
+                i++;
+                continue;
+            }
+
+
+            try {
+                await connection.execute('delete from monitor_shop where productName=? and shop=?', [productName[0], 'PcHouse']);
+            } catch (error) {
+                console.error(error);
+            }
+
+
+            try {
+                const [rows] = await connection.execute('select * from monitor_details where productName=?', [productName[0]]) || [];
+                if (rows[0] == undefined) {
+
+                    await connection.execute('INSERT INTO monitor_details (productName, resolution, displaySize, panelType, image, description) VALUES (?, ?, ?, ?, ?, ?)', [productName[0] || "", resolution || "", displaySize || 0, panelType || "", photos[i++] || "", description[0] || ""]);
+                }
+            } catch (error) {
+                console.error(error);
+            }
+
+            try {
+                await connection.execute('INSERT INTO monitor_shop (productName, price, shop, link, warranty) VALUES (?, ?, ?, ?, ?)', [productName[0], price[0], 'PcHouse', link, warranty]);
+            } catch (error) {
+                console.error(error);
+            }
+        }
+        catch (error) {
+            console.error(error);
+            console.log('monitor-pcHouse');
+            i++;
+        }
+
+    }
+    console.log('Pdone');
+    browser.close();
+}
+
+
+
+
+
 
 
 
